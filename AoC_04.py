ac39cd9e8405198e3bec95fc743eb26490acb35b
IgorB <90834134+ibrusett@users.noreply.github.com>
2022-12-04 13:55:27 +0100

    Update AoC_04.py

--- AoC_04.py
+++ AoC_04.py
@@ -23,15 +23,3 @@ for element in lista:
 
 print("Prima parte: ", ans1)
 print("Seconda parte: ", ans2)
-
-# si poteva fare creando dei range
-irange = lambda a, b: range(a, b + 1)  # funzione monoriga definita come lambda [parametri]: funzione
-ans3 = 0
-for element in lista:
-    x, y = element.strip().split(",")  # assegno a due variabili x e y le due parti dello split
-    a = set(irange(*map(int, x.split("-"))))  # set che contiene gli elementi del primo range
-    b = set(irange(*map(int, y.split("-"))))  # set che contiene gli elementi del secondo range
-    # print(x, y, a, b, len(a & b))
-    if (len(a & b) > 0):
-        ans3 +=1
-print("seconda parte coi range: ", ans3)
